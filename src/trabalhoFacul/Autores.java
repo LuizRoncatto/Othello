89bd0acf6729da08fdcbf9e4f19d066e43597872
Suporte <suporte@wspot.com.br>
2019-10-16 18:59:16 -0300

    nome completo do Leonardo

--- src/trabalhoFacul/Autores.java
+++ src/trabalhoFacul/Autores.java
@@ -8,7 +8,7 @@ public class Autores {
 
 	public void imprimirNomes() {
 		System.out.println("Luiz Fernando de Oliveira Roncatto  RA: 142779412002  Eng. Computação"); 
-		System.out.println("Leonardo Padilha Ramos  RA:  Eng. Computação");
+		System.out.println("Leonardo Padilha Ramos de Souza  RA:   Eng. Computação");
 	}
 
 }
