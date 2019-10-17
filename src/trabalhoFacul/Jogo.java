898b0803635fce92d6fcb97f92e189ca6609224a
LuizRoncatto <56552106+LuizRoncatto@users.noreply.github.com>
2019-10-16 23:18:11 -0300

    Develop (#8)
    * primeiro javadoc class Othello - main
    
    * nome completo do Leonardo
    
    * Alterações feitas em sala, criação do método jogo init, sem validações

--- src/trabalhoFacul/Jogo.java
+++ src/trabalhoFacul/Jogo.java
@@ -1,18 +1,39 @@
 package trabalhoFacul;
 
-public class Jogo {
-	
-	protected boolean jogador = true;
-	
-	public Jogo (){
-		
-	}
-	
-	public void jogadorJogador() {
-		
-	}
-	
+import java.util.Scanner;
 
-	
+public class Jogo {
+
+	protected boolean jogador = true;
+
+	public Jogo() {
+
+	}
+
+	public void initJogo() {
+
+		Tabuleiro tabuleiro = new Tabuleiro();
+		tabuleiro.imprimir();
+
+		Scanner entrada = new Scanner(System.in);
+
+		do{
+			System.out.println("faça sua jogada");
+		
+		System.out.println("Digite a linha");
+		int linha = entrada.nextInt();
+		System.out.println("Digite a coluna");
+		int coluna = entrada.nextInt();
+
+		tabuleiro.jogada(linha, coluna, jogador);
+
+		tabuleiro.imprimir();
+
+		jogador = !jogador;
+		}while(true);
+		
+		
+
+	}
 
 }
