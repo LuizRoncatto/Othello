24df81d554b9200ae11e165365159ff9f3df2202
LuizRoncatto <56552106+LuizRoncatto@users.noreply.github.com>
2019-10-21 14:19:56 -0300

    validação de existência de peça na jogada feita (#17)
    * validação de existencia de peça na jogada feita
    
    * RESOLVENDO CONFLITO

--- src/trabalhoFacul/Peca.java
+++ src/trabalhoFacul/Peca.java
@@ -1,13 +1,13 @@
 package trabalhoFacul;
 
 public class Peca {
-	
+
 	protected static String PecaX = "X";
 	protected static String PecaO = "O";
 	protected static String PecaVazia = "-";
-	
+
 	protected String caracter;
-	
+
 	public Peca(String caracter) {
 		this.caracter = caracter;
 	}
@@ -19,8 +19,22 @@ public class Peca {
 	public void setCaracter(String caracter) {
 		this.caracter = caracter;
 	}
-	
-	
-	
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Peca other = (Peca) obj;
+		if (caracter == null) {
+			if (other.caracter != null)
+				return false;
+		} else if (!caracter.equals(other.caracter))
+			return false;
+		return true;
+	}
 
 }
--- src/trabalhoFacul/Regras.java
+++ src/trabalhoFacul/Regras.java
@@ -1,17 +1,18 @@
 package trabalhoFacul;
 
 public class Regras {
-	public Regras(){
-		
+	public Regras() {
+
 	}
-	
-	public void imprimir(){
-		System.out.printf(" O Othello (Reversi) é disputado entre dois jogadores em um tabuleiro 8x8 com 64 discos que possui uma cor escura em um dos lados da face e no outro uma cor clara,cujo o objetivo é ficar com mais peças da sua cor no tabuleiro ao final do jogo."
-				+ "\n A partida começa com 4 peças no centro do tabuleiro, duas brancas e duas pretas"
-				+ ", \nsendo sempre as brancas na grande diagonal a1-h8.\nO jogador com as pretas começa a partida colocando uma peça em uma das  quatro opções marcadas no tabuleiro ao lado."
-				+ "\nUma vez que um jogador faça uma jogada válida, todas as peças do oponente que estejam em uma linha reta (horizontal, vertical ou diagonal) entre a peça recém-colocada e qualquer outra peça do jogador que fez o movimento são viradas e passam a ser da cor deste jogador."
-				+ "\nO jogo termina quando nenhum dos jogadores puder fazer movimentos (por exemplo, quando todas as casas estiverem ocupadas ou todas as peças do tabuleiro estiverem com a mesma cor virada para cima."
-				+ "\nSe um jogador possuir mais peças que o adversário, ele vence. Se ambos os jogadores tiverem a mesma quantidade de peças, o jogo termina em empate.\n");
+
+	public void imprimir() {
+		System.out.printf(
+				" O Othello (Reversi) é disputado entre dois jogadores em um tabuleiro 8x8 com 64 discos que possui uma cor escura em um dos lados da face e no outro uma cor clara,cujo o objetivo é ficar com mais peças da sua cor no tabuleiro ao final do jogo."
+						+ "\n A partida começa com 4 peças no centro do tabuleiro, duas brancas e duas pretas"
+						+ ", \nsendo sempre as brancas na grande diagonal a1-h8.\nO jogador com as pretas começa a partida colocando uma peça em uma das  quatro opções marcadas no tabuleiro ao lado."
+						+ "\nUma vez que um jogador faça uma jogada válida, todas as peças do oponente que estejam em uma linha reta (horizontal, vertical ou diagonal) entre a peça recém-colocada e qualquer outra peça do jogador que fez o movimento são viradas e passam a ser da cor deste jogador."
+						+ "\nO jogo termina quando nenhum dos jogadores puder fazer movimentos (por exemplo, quando todas as casas estiverem ocupadas ou todas as peças do tabuleiro estiverem com a mesma cor virada para cima."
+						+ "\nSe um jogador possuir mais peças que o adversário, ele vence. Se ambos os jogadores tiverem a mesma quantidade de peças, o jogo termina em empate.\n");
 
 	}
 
