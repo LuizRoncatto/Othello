2cfc04c209392484e3cecf63ad5ae5559a416bb3
Suporte <suporte@wspot.com.br>
2019-10-21 14:11:31 -0300

    validação de existencia de peça na jogada feita

--- src/trabalhoFacul/PecaO.java
+++ src/trabalhoFacul/PecaO.java
@@ -1,9 +1,9 @@
 package trabalhoFacul;
 
-public class PecaO extends Peca{
-	
-	public PecaO(){
-		super (Peca.PecaO);
+public class PecaO extends Peca {
+
+	public PecaO() {
+		super(Peca.PecaO);
 	}
 
 }
