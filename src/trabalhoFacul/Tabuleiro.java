4097d0c1c5c9e2e560807fb1ed1759e520bf66a0
Suporte <suporte@wspot.com.br>
2019-10-24 00:23:12 -0300

    adicionado numeros laterais para guiar nas jogadas, adicionado conversão para jogadas serem baseadas nos numeros laterais. Criação da transformação de peças na vertical indo de cima para baixo

--- src/trabalhoFacul/Tabuleiro.java
+++ src/trabalhoFacul/Tabuleiro.java
@@ -6,6 +6,7 @@ public class Tabuleiro {
 	private Peca tabuleiro[][];
 
 	public Tabuleiro() {
+
 		tabuleiro = new Peca[Tam_Tabuleiro][Tam_Tabuleiro];
 
 		for (int i = 0; i < Tam_Tabuleiro; i++) {
@@ -25,13 +26,17 @@ public class Tabuleiro {
 	}
 
 	public void imprimir() {
+
+		System.out.println(" " + 12345678);
 		for (int i = 0; i < Tam_Tabuleiro; i++) {
+			System.out.print(i + 1);
 			for (int j = 0; j < Tam_Tabuleiro; j++) {
 
 				System.out.printf(tabuleiro[i][j].getCaracter());
 
 			}
 			System.out.printf("\n");
+
 		}
 	}
 
@@ -52,6 +57,30 @@ public class Tabuleiro {
 			return true;
 		} else
 			return false;
+
+	}
+
+	public void convertePecaVertical(int linha, int coluna, boolean jogador) {
+		if (jogador == true) {
+			int mudei = 0;
+			boolean acheiPeca = false;
+			for (int i = linha+1 ; i < Tam_Tabuleiro; i++) {
+				if (tabuleiro[i][coluna].getCaracter().equals(Peca.PecaO)) {
+					acheiPeca = true;
+				} else {
+					mudei++;
+				}
+				
+				if(acheiPeca==true && mudei > 0) {
+					for(int line = linha+1; !tabuleiro[line][coluna].getCaracter().equals(Peca.PecaO);line++)
+					tabuleiro[line][coluna] = new PecaO();
+				}
+
+			}
+			
+			
+
+		}
 
 	}
 
