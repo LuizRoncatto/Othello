0a612fc255dc7161a7bad6b380c6a237bd29f57c
Suporte <suporte@wspot.com.br>
2019-11-12 14:03:28 -0300

    mudança diagonais; validação jogada dentro do tabuleiro

--- src/trabalhoFacul/Jogo.java
+++ src/trabalhoFacul/Jogo.java
@@ -1,5 +1,6 @@
 package trabalhoFacul;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Jogo {
@@ -18,29 +19,31 @@ public class Jogo {
 		Scanner entrada = new Scanner(System.in);
 
 		do {
-			System.out.println("fa�a sua jogada");
+			System.out.println("faca sua jogada");
 
 			System.out.println("Digite a linha");
-			int linha = entrada.nextInt()-1;
+
+			int linha = entrada.nextInt() - 1;
+
 			System.out.println("Digite a coluna");
-			int coluna = entrada.nextInt()-1;
+			int coluna = entrada.nextInt() - 1;
 
-			if (tabuleiro.verificaEspaco(linha, coluna) == true) {
-
-			} else {
+			if (tabuleiro.verificaEspaco(linha, coluna) == false) {
 				tabuleiro.jogada(linha, coluna, jogador);
 
 				tabuleiro.convertePecaVertical(linha, coluna, jogador);
 				tabuleiro.convertePecaHorizontal(linha, coluna, jogador);
-
+				tabuleiro.converteDiagonal(linha, coluna, jogador);
 				jogador = !jogador;
 				// fimDeJogo ++;
 			}
+
 			tabuleiro.imprimir();
 
 			tabuleiro.contaJogada();
+
 		} while (tabuleiro.contador < 60);
-		//entrada.close();
+		// entrada.close();
 
 	}
 
